49cd4ee78c05db13d7988e77fa1e131ba5ab3283
zurgeg <46549042+zurgeg@users.noreply.github.com>
2019-10-06 10:51:47 -0400

    Load Mesage/Stage 01/Splash Screen

---
+++ 02-bootsector-print/load_msg_boot_sector.asm
@@ -0,0 +1,31 @@
+mov ah, 0x0e ; tty mode
+
+mov al, 'G'
+
+int 0x10
+
+mov al, 'B'
+
+int 0x10
+
+mov al, 'T'
+
+int 0x10
+
+int 0x10 ; 'O' is still on al, remember?
+
+mov al, 'S'
+
+int 0x10
+
+
+
+jmp $ ; jump to current address = infinite loop
+
+
+
+; padding and magic number
+
+times 510 - ($-$$) db 0
+
+dw 0xaa55 
