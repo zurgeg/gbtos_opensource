30f45cd88472a010f3ead9c297e7ca8bc404f9ee
zurgeg <46549042+zurgeg@users.noreply.github.com>
2019-11-28 11:24:15 -0500

    Add tests and boot nofication
    Add some stuff. I now realise why everything is abbreveated

--- 02-bootsector-print/boot_sect_hello.asm
+++ 02-bootsector-print/boot_sect_hello.asm
@@ -8,6 +8,62 @@ int 0x10
 int 0x10 ; 'l' is still on al, remember?
 mov al, 'o'
 int 0x10
+mov al, '\n'
+mov al, 'I'
+int 0x10
+mov al, 'M'
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 'd'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, '\n'
+int 0x10
+mov al, 'P'
+int 0x10 
+mov al, 'W'
+int 0x10
+mov al, 'R'
+int 0x10 
+mov al, 'F'
+int 0x10
+mov al, 'A'
+int 0x10
+mov al, 'I'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'I'
+int 0x10
+mov al, 'F'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'R'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'T'
+int 0x10
+
+
+
 
 jmp $ ; jump to current address = infinite loop
 
