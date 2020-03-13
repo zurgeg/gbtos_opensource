081b835be65f4170d5f5e5c47dfa78bc3207e031
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-13 10:41:15 -0400

    Create bootloader.asm

---
+++ micro/bootloader.asm
@@ -0,0 +1,19 @@
+mov ah, 0x0e
+mov al, '0'
+int 0x10
+mov al, '0'
+int 0x10
+mov al, '0'
+int 0x10
+mov al, '0'
+int 0x10
+mov al, 'B'
+int 0x10
+mov al, 'C'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'D'
+int 0x10
+mov al, 'E'
+int 0x10
