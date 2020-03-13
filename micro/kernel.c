9dc299396d5e80e04a631ffd97e1c0b10b582808
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-13 10:50:50 -0400

    Update kernel.c

--- micro/kernel.c
+++ micro/kernel.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+#include <sysutils.h>
 int main() {
+   char str[MEM_VAL]
    /* my first program in C */
    printf("\nStarting Up...\n");
+   c = getchar( )
    
    
    return 0;
