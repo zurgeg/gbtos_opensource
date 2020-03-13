f3305e0ba0a179651d0822cb2da737998f2e968b
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-13 11:19:22 -0400

    Update kernel.c

--- micro/kernel.c
+++ micro/kernel.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "sysutils.h"
+#include "lib/gui.h"
 int main() {
    char str[MEM_VAL];
    /* my first program in C */
    printf("\nStarting Up...\n");
-   c = get( str )
-   put( c )
+   //c = get( str )
+   //put( c )
+   *txt = 'T'
+   *txt = 'E'
+   *txt = 'S'
+   *txt = 'T'
       
    
    
